53e4224ec455cd0000c71cc87bb0b49eb9fc251b
DanWillmore <danWillmore@gmail.com>
2025-01-10 21:00:01 +0000

    commit

--- static/js/app.js
+++ static/js/app.js
@@ -24,32 +24,63 @@ var tooltip = d3.select('body')
 
 // Read and arrange the data
 
-
-
+jsonParse(await FileAttachment("data.json").text())
 
 // Set scales
 
-
-
+var xScale = d3.scaleBand().range([0, width]).padding(0.5),
+            yScale = d3.scaleLinear().range([height, 0]);
 
 // Create axes
 
-
+xScale.domain(dataset1);
+        yScale.domain([0, 100]);
 
 // Append the axes as G
 
+g.append("g")
+         .attr("transform", "translate(0," + height + ")")
+         .call(d3.axisBottom(xScale).tickFormat(function(d){
+           return "Wins: " + d;
+         })
+         );
 
+        g.append("g")
+         .call(d3.axisLeft(yScale).tickFormat(function(d){
+             return "$" + d;
+         }).ticks(4));
 
 
 
 // Create Bars or Line function
 
+g.selectAll(".bar")
+         .data(dataset1)
+         .enter().append("rect")
 
-
+         g.selectAll(".bar")
+         .data(dataset1)
+         .enter().append("rect")
+         .attr("class", "bar")
+         .attr("x", function(d) { return xScale(d); })
+         .attr("y", function(d) { return yScale(d); })
+         .attr("width", xScale.bandwidth())
+         .attr("height", function(d) { return height - yScale(d); });
 
 // Create Circle group
 
+svg.append("circle").attr("cx",200).attr("cy",130).attr("r", 6).style("fill", "#69b3a2")
+svg.append("circle").attr("cx",200).attr("cy",160).attr("r", 6).style("fill", "#404080")
+svg.append("circle").attr("cx",200).attr("cy",190).attr("r", 6).style("fill", "#ffff00")
 
+// select the svg area
+var svg = d3.select("#my_dataviz")
+
+// Handmade legend
+
+svg.append("text").attr("x", 220).attr("y", 130).text("Wins").style("font-size", "15px").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 220).attr("y", 160).text("Losses").style("font-size", "15px").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 220).attr("y", 190).text("Draws").style("font-size", "15px").attr("alignment-baseline","middle")
 
 
 // Mouseover / mouseout
@@ -58,6 +89,17 @@ var tooltip = d3.select('body')
 
 // tooltip
 
+// create a tooltip
+var tooltip = d3.select("#my_dataviz")
+  .append("div")
+    .style("position", "absolute")
+    .style("visibility", "hidden")
 
+    d3.select("#circleBasicTooltip")
+    .on("mouseover", function(){return tooltip.style("visibility", "visible");})
+    .on("mousemove", function(){return tooltip.style("top", (event.pageY-800)+"px").style("left",(event.pageX-800)+"px");})
+    .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 
 // Legend function
+
+  <svg id="my_dataviz" height=300 width=450></svg>
